690db6c75d909e5570e43a31bcb165aad59ff6b0
mozovw <408917611@qq.com>
2016-12-03 12:09:29 +0800

    ssm
    springmvc spring mybatis

---
+++ ssm-parent/ssm-test/src/main/java/com/mozovw/ssm/test/App.java
@@ -0,0 +1,13 @@
+package com.mozovw.ssm.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
