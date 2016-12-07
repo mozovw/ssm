ee28bb823a5895f1f2d0afe3558a9a0fe6a2db8f
frank <408917611@qq.com>
2016-12-07 15:40:03 +0800

    1207 add ssm-mybatis-oracle

--- ssm-parent/ssm-mybatis-mysql/src/main/java/com/mozovw/ssm/mybatis/mysql/annotation/service/AnnotaionService.java
+++ ssm-parent/ssm-mybatis-mysql/src/main/java/com/mozovw/ssm/mybatis/mysql/annotation/service/AnnotaionService.java
@@ -8,16 +8,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.mozovw.ssm.core.common.entity.EmpEntity;
 import com.mozovw.ssm.mybatis.mysql.annotation.dao.AnnotaionDao;
-import com.mozovw.ssm.mybatis.mysql.noannotation.service.INoAnnotaionService;
 
 @Service
 @Transactional(readOnly = true)
-public class AnnotaionService implements INoAnnotaionService {
+public class AnnotaionService  {
 	
 	@Autowired
 	private AnnotaionDao annotaionDao;
 	
-	@Override
 	public List<EmpEntity> findAllEmp() {
 		return annotaionDao.findAllEmp();
 	}
